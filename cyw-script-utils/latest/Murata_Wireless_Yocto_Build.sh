6fff6e50e5f60b93f412f65890a847cd862f14a1
jameelkareem-mothra <jameelkareem.mothra@gmail.com>
2018-08-14 17:37:48 -0500

    Updated for pre-release

--- cyw-script-utils/latest/Murata_Wireless_Yocto_Build.sh
+++ cyw-script-utils/latest/Murata_Wireless_Yocto_Build.sh
@@ -48,8 +48,8 @@ echo "2) Verifying Script Version"
 echo "---------------------------"
 
 
-GITHUB_PATH="\""https://github.com/jameelkareem-rocko/meta-murata-wireless.git"\""
-META_MURATA_WIRELESS_GIT="https://github.com/jameelkareem-rocko/meta-murata-wireless.git"
+GITHUB_PATH="\""https://github.com/jameelkareem-pre-release-rocko/meta-murata-wireless.git"\""
+META_MURATA_WIRELESS_GIT="https://github.com/jameelkareem-pre-release-rocko/meta-murata-wireless.git"
 
 echo "Fetching latest script from Murata Github."
 echo "Cloning $GITHUB_PATH"
